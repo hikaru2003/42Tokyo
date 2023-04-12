e3e3cf90413f6572c91202153cef04dcea900b76
hikaru <hikaru.morisaki.0316@gmail.com>
2023-04-12 11:56:17 +0900

    solong windows position top-left

--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -27,8 +27,9 @@ int	main(int argc, char **argv)
 	data->head = creat_map(check_file(argv[1]), data);
 	if (check_map(data) != 0)
 		exit(1);
-	data->win = mlx_new_window(data->mlx, data->width * 32,
-			data->height * 32, "so_long");
+	// data->win = mlx_new_window(data->mlx, data->width * 32,
+	// 		data->height * 32, "so_long");
+	data->win = mlx_new_window(data->mlx, 0, 0, "so_long");
 	mlx_hook(data->win, 2, 1, next_frame, data);
 	mlx_hook(data->win, 17, 1, ft_destroy, data);
 	mlx_loop_hook(data->mlx, draw_map, data);
