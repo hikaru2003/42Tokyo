a7d7a1592327b7902ee23f6ef6c63a4eb2797d75
森崎 光 <hikaru@hikaruMacBook-Air.local>
2023-04-29 09:11:16 +0900

    exam

---
+++ exam/sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hikaru <hikaru@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 08:55:57 by hikaru            #+#    #+#             */
+/*   Updated: 2023/04/29 09:00:18 by hikaru           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	int				tmp;
+
+	i = 0;
+	j = 0;
+	tmp = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
