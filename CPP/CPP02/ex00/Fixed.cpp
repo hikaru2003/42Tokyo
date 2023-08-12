4e8878ebe5747feef8d8f45e306f1214bee51a04
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2023-08-12 19:47:36 +0900

    ex00

--- CPP/CPP02/ex00/Fixed.cpp
+++ CPP/CPP02/ex00/Fixed.cpp
@@ -6,8 +6,45 @@
 /*   By: hikaru <hikaru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 14:30:35 by hikaru            #+#    #+#             */
-/*   Updated: 2023/08/12 14:30:43 by hikaru           ###   ########.fr       */
+/*   Updated: 2023/08/12 19:36:41 by hikaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed(/* args */)
+{
+	std::cout << "Default constructor called" << std::endl;
+	fp_number_ = 0;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->fp_number_ = fixed.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &fixed)
+		this->fp_number_ = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->fp_number_;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fp_number_ = raw;
+}
--- CPP/CPP02/ex00/Fixed.hpp
+++ CPP/CPP02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hikaru <hikaru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 14:30:33 by hikaru            #+#    #+#             */
-/*   Updated: 2023/08/12 14:31:24 by hikaru           ###   ########.fr       */
+/*   Updated: 2023/08/12 18:34:49 by hikaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,14 @@
 class Fixed
 {
 private:
-	/* data */
+	int fp_number_;
+	static const int bits_;
+	
 public:
 	Fixed(/* args */);
+	Fixed(const Fixed &fixed); //コピーコンストラクタ
+	Fixed &operator=(const Fixed &fixed); //コピー代入演算子
 	~Fixed();
+	int	getRawBits(void) const;
+	void setRawBits(int const raw);
 };
-
-Fixed::Fixed(/* args */)
-{
-}
-
-Fixed::~Fixed()
-{
-}
-
